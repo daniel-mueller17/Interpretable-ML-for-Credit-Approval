9d008c9f0844830186a3b47dffe0d9303c16251f
Daniel Müller <daniel._mueller@arcor.de>
2025-05-27 18:23:17 +0200

    new preprocessing

--- code/preprocess_data.R
+++ code/preprocess_data.R
@@ -16,38 +16,28 @@ data_raw <- read.csv("./data/data_raw/actions_taken_1-3_state_DC.csv") # 11531 o
 data <- data_raw %>% 
   select(
     action_taken,
-    applicant_credit_score_type,
     co.applicant_credit_score_type,
     income,
     debt_to_income_ratio,
-    applicant_age,
+    applicant_age_above_62,
     applicant_ethnicity.1,
     applicant_race.1,
     applicant_sex,
     loan_amount,
-    loan_to_value_ratio,
     interest_rate,
     rate_spread,
     loan_type,
     loan_purpose,
-    loan_term,
     origination_charges,
     total_loan_costs,
     discount_points,
     property_value,
-    total_units,
     occupancy_type,
     conforming_loan_limit,
-    tract_population,
-    tract_minority_population_percent,
     tract_to_msa_income_percentage,
-    tract_owner_occupied_units,
-    tract_median_age_of_housing_units,
-    submission_of_application,
-    business_or_commercial_purpose,
     preapproval,
     lien_status,
-    open.end_line_of_credit
+    total_units
   )
 
 # We create a new variable "has_co.applicant"
@@ -66,6 +56,7 @@ data <- data %>%
   group_by(tract_to_msa_income_percentage) %>% 
   mutate(income = if_else(is.na(income), median(income, na.rm = TRUE), income)) %>%
   ungroup()
+data$tract_to_msa_income_percentage <- NULL # Not needed anymore
 
 sum(is.na(data$debt_to_income_ratio)) # 855 NAs -> new category "unknown"
 data$debt_to_income_ratio <- replace_na(data$debt_to_income_ratio, "unknown")
@@ -73,30 +64,19 @@ sum(data$debt_to_income_ratio == "Exempt") # 144 obs. -> also unknown
 data <- data %>%  mutate(
   debt_to_income_ratio = if_else(debt_to_income_ratio == "Exempt", "unknown", debt_to_income_ratio))
 
-sum(data$loan_term == "Exempt", na.rm = TRUE) # 144 obs. -> remove
-data <- data %>% 
-  filter(loan_term != "Exempt")
-sum(is.na(data$loan_term))
-data$loan_term <- as.numeric(data$loan_term)
-data$loan_term <- replace_na(data$loan_term, median(data$loan_term, na.rm = TRUE))
-
 sum(is.na(data$property_value)) # 419 NAs -> impute values with median groubed by "total_units"
 data$property_value <- as.numeric(data$property_value)
 data <- data %>% 
   group_by(total_units) %>% 
   mutate(property_value = if_else(is.na(property_value), median(property_value, na.rm = TRUE), property_value)) %>%
   ungroup()
+data$total_units <- NULL # Not needed anymore
 
 sum(is.na(data$applicant_ethnicity.1)) # 5 NAs -> remove
 data <- drop_na(data, applicant_ethnicity.1)
 
 sum(is.na(data$applicant_race.1)) # 2 NAs -> remove
 data <- drop_na(data, applicant_race.1)
-
-sum(is.na(data$loan_to_value_ratio)) # 512 NAs -> calculate values with loan_amount/property_value
-data$loan_to_value_ratio <- as.numeric(data$loan_to_value_ratio)
-data <- data %>% 
-  mutate(loan_to_value_ratio = if_else(is.na(loan_to_value_ratio), (loan_amount/property_value)*100, loan_to_value_ratio))
 
 sum(is.na(data$interest_rate)) # 2807 NAs -> variable not really usable -> remove column
 data$interest_rate <- NULL
@@ -116,6 +96,9 @@ data$discount_points <- NULL
 sum(is.na(data$conforming_loan_limit)) # 138 NAs -> new category "unknown"
 data$conforming_loan_limit <- replace_na(data$conforming_loan_limit, "unknown")
 
+sum(is.na(data$applicant_age_above_62)) # 420 NAs -> remove Data
+data <- drop_na(data, applicant_age_above_62)
+
 dim(data) # 11244 observations and 29 variables
 
 
@@ -130,10 +113,6 @@ data <- data %>%
                                         if_else(loan_purpose == 31, "Refinancing",
                                                 if_else(loan_purpose == 32, "Cash-out refinancing", "Other purpose")))),
          lien_status = if_else(lien_status == 1, "Secured by a first lien", "Secured by a subordinate lien"),
-         open.end_line_of_credit = if_else(open.end_line_of_credit == 1, "Open-end line of credit", "Not open-end line of credit"),
-         business_or_commercial_purpose = if_else(business_or_commercial_purpose == 1,
-                                                  "Primarily for a business or commercial purpose",
-                                                  "Not primarily for a business or commercial purpose"),
          occupancy_type = if_else(occupancy_type == 1, "Principal residence",
                                   if_else(occupancy_type == 2, "Second residence", "Investment property")),
          debt_to_income_ratio = if_else(debt_to_income_ratio %in% c("36", "37", "38", "39", "40", "41", "42"), "36%-42%",
@@ -147,10 +126,7 @@ data <- data %>%
                                     if_else(applicant_race.1 == 3, "Black or African American",
                                             if_else(applicant_race.1 == 5, "White", "other"))),
          applicant_sex = if_else(applicant_sex %in% c(3, 4, 6), "unknown",
-                                 if_else(applicant_sex == 1, "Male", "Female")),
-         submission_of_application = if_else(submission_of_application == 1, "Submitted directly", "Not submitted directly"),
-         applicant_credit_score_type = if_else(applicant_credit_score_type %in% c(4, 5, 6), 8,
-                                               if_else(applicant_credit_score_type %in% c(9, 11), 9, applicant_credit_score_type))
+                                 if_else(applicant_sex == 1, "Male", "Female"))
   ) %>% 
   rename(
     applicant_ethnicity = applicant_ethnicity.1,
@@ -162,25 +138,20 @@ data <- data %>%
     applicant_ethnicity = as.factor(applicant_ethnicity),
     applicant_race = as.factor(applicant_race),
     applicant_sex = as.factor(applicant_sex),
+    applicant_age_above_62 = as.factor(applicant_age_above_62),
     action_taken = as.factor(action_taken),
     preapproval = as.factor(preapproval),
     loan_type = as.factor(loan_type),
     loan_purpose = as.factor(loan_purpose),
     lien_status = as.factor(lien_status),
-    open.end_line_of_credit = as.factor(open.end_line_of_credit),
-    business_or_commercial_purpose = as.factor(business_or_commercial_purpose),
     occupancy_type = as.factor(occupancy_type),
-    total_units = as.factor(total_units),
     debt_to_income_ratio = factor(debt_to_income_ratio,
                                   levels = c("<20%", "20%-29%", "30%-35%", "36%-42%", "43%-49%", "50%-60%", ">60%", "unknown")),
-    applicant_credit_score_type = as.factor(applicant_credit_score_type),
-    applicant_age = as.factor(applicant_age),
     income = as.numeric(income),
     loan_amount = as.numeric(loan_amount),
     property_value = as.numeric(property_value),
     has_co.applicant = as.factor(has_co.applicant),
-    conforming_loan_limit = as.factor(conforming_loan_limit),
-    submission_of_application = as.factor(submission_of_application)
+    conforming_loan_limit = as.factor(conforming_loan_limit)
   )
 
 
