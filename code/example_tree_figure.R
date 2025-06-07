371d3c856b9b76d4f45c8ffb8d0c58078df1a69b
Daniel Müller <daniel._mueller@arcor.de>
2025-06-07 18:27:22 +0200

    tree example figure

---
+++ code/example_tree_figure.R
@@ -0,0 +1,15 @@
+
+library(rpart)
+library(rpart.plot)
+
+# Read data
+data <- readRDS("./data/credit_approval_data.rds")
+
+# Set seed for reproducibility
+set.seed(123)
+
+# Train Tree
+tree <- rpart(action_taken ~ ., data = data, method = "class")
+
+# Plot Tree
+rpart.plot(tree, main = "Decision Tree", extra = 4)
