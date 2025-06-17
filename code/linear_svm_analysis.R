bb01b8d1868c9ef1d302f4737ae020c48463f9ed
Daniel Müller <daniel._mueller@arcor.de>
2025-06-17 17:19:39 +0200

    effect linear svm comparison

--- code/linear_svm_analysis.R
+++ code/linear_svm_analysis.R
@@ -294,13 +294,18 @@ ggsave(file = "./plots/linear_svm/effect_debt.pdf", plot = effect_plot_debt)
 ggsave(file = "./plots/linear_svm/effect_purpose.pdf", plot = effect_plot_purpose)
 
 # Save effect data for comparison
-write.csv(effect_debt$results, "./data/feature_effects/linear_svm/debt.csv")
-write.csv(effect_purpose$results, "./data/feature_effects/linear_svm/purpose.csv")
-write.csv(effect_pre$results, "./data/feature_effects/linear_svm/pre.csv")
-write.csv(effect_amount$results, "./data/feature_effects/linear_svm/amount.csv")
-write.csv(effect_lien$results, "./data/feature_effects/linear_svm/lien.csv")
-write.csv(effect_co$results, "./data/feature_effects/linear_svm/co.csv")
-write.csv(effect_income$results, "./data/feature_effects/linear_svm/income.csv")
-write.csv(effect_race$results, "./data/feature_effects/linear_svm/race.csv")
-write.csv(effect_eth$results, "./data/feature_effects/linear_svm/eth.csv")
-write.csv(effect_sex$results, "./data/feature_effects/linear_svm/sex.csv")
+list_effects_old <- list(effect_debt$results, effect_purpose$results, effect_pre$results, effect_amount$results, effect_lien$results,
+                         effect_co$results, effect_income$results, effect_race$results, effect_eth$results, effect_sex$results)
+
+list_effects <- lapply(list_effects_old, function(x) cbind(x, Model = "Linear SVM"))
+
+write.csv(list_effects[[1]], "./data/feature_effects/linear_svm/debt.csv")
+write.csv(list_effects[[2]], "./data/feature_effects/linear_svm/purpose.csv")
+write.csv(list_effects[[3]], "./data/feature_effects/linear_svm/pre.csv")
+write.csv(list_effects[[4]], "./data/feature_effects/linear_svm/amount.csv")
+write.csv(list_effects[[5]], "./data/feature_effects/linear_svm/lien.csv")
+write.csv(list_effects[[6]], "./data/feature_effects/linear_svm/co.csv")
+write.csv(list_effects[[7]], "./data/feature_effects/linear_svm/income.csv")
+write.csv(list_effects[[8]], "./data/feature_effects/linear_svm/race.csv")
+write.csv(list_effects[[9]], "./data/feature_effects/linear_svm/eth.csv")
+write.csv(list_effects[[10]], "./data/feature_effects/linear_svm/sex.csv")
