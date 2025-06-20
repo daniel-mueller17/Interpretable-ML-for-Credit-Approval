593583a09477c0a6e0af06488b5453f8b7f3bcc9
Daniel Müller <daniel._mueller@arcor.de>
2025-06-20 15:02:44 +0200

    effects linear comparison

--- code/linear_model_analysis.R
+++ code/linear_model_analysis.R
@@ -207,6 +207,14 @@ effect_plot_eth <- effect_eth$results %>%
   facet_wrap(~"Loan approved")
 effect_plot_eth
 
+effect_type <- FeatureEffect$new(predictor, feature = "loan_type", method = "pdp")
+effect_plot_type <- effect_type$results %>% 
+  filter(.class == "Loan approved") %>% 
+  ggplot(aes(x = loan_type, y = .value)) +
+  geom_col(fill = "steelblue") +
+  facet_wrap(~"Loan approved")
+effect_plot_type
+
 # Save plots
 ggsave(file = "./plots/linear_model/effect_debt.pdf", plot = effect_plot_debt)
 ggsave(file = "./plots/linear_model/effect_purpose.pdf", plot = effect_plot_purpose)
@@ -215,7 +223,7 @@ ggsave(file = "./plots/linear_model/effect_preapproval.pdf", plot = effect_plot_
 
 # Save effect data for comparison
 list_effects_old <- list(effect_debt$results, effect_purpose$results, effect_pre$results, effect_amount$results, effect_lien$results,
-                       effect_co$results, effect_income$results, effect_race$results, effect_eth$results, effect_sex$results)
+                       effect_co$results, effect_income$results, effect_race$results, effect_eth$results, effect_sex$results, effect_type$results)
 
 list_effects <- lapply(list_effects_old, function(x) cbind(x, Model = "Logistic Regression"))
 
@@ -229,3 +237,4 @@ write.csv(list_effects[[7]], "./data/feature_effects/linear_model/income.csv")
 write.csv(list_effects[[8]], "./data/feature_effects/linear_model/race.csv")
 write.csv(list_effects[[9]], "./data/feature_effects/linear_model/eth.csv")
 write.csv(list_effects[[10]], "./data/feature_effects/linear_model/sex.csv")
+write.csv(list_effects[[11]], "./data/feature_effects/linear_model/type.csv")
