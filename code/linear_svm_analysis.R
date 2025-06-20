4d39a52c96954aed681b248015b70af97035d979
Daniel Müller <daniel._mueller@arcor.de>
2025-06-20 15:23:59 +0200

    effect linear SVM comparison

--- code/linear_svm_analysis.R
+++ code/linear_svm_analysis.R
@@ -289,14 +289,6 @@ effect_plot_property <- effect_property$results %>%
   facet_wrap(~"Loan approved")
 effect_plot_property
 
-effect_type <- FeatureEffect$new(predictor, feature = "loan_type", method = "pdp")
-effect_plot_type <- effect_type$results %>% 
-  filter(.class == "Loan approved") %>% 
-  ggplot(aes(x = loan_type, y = .value)) +
-  geom_col(fill = "steelblue") +
-  facet_wrap(~"Loan approved")
-effect_plot_type
-
 # Save plots
 ggsave(file = "./plots/linear_svm/effect_debt.pdf", plot = effect_plot_debt)
 ggsave(file = "./plots/linear_svm/effect_purpose.pdf", plot = effect_plot_purpose)
